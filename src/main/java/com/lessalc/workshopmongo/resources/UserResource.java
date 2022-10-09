2c3b8fc5fc7699e4abab78f606563727e03b4627
Lessalc <lucianoclessa@gmail.com>
2022-10-09 18:57:28 -0300

    Handling exceptions

--- src/main/java/com/lessalc/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lessalc/workshopmongo/resources/UserResource.java
@@ -3,8 +3,12 @@ package com.lessalc.workshopmongo.resources;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +30,12 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> collect = list.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(collect);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 	
 }
--- src/main/java/com/lessalc/workshopmongo/services/UserService.java
+++ src/main/java/com/lessalc/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lessalc.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lessalc.workshopmongo.domain.User;
 import com.lessalc.workshopmongo.repository.UserRepository;
+import com.lessalc.workshopmongo.services.exception.ObjectNotFoundException;
 
 
 @Service
@@ -17,5 +19,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
 	}
 }
