cbfd4881d2ebc3a4aa7eca80a62d58f972cb0bf6
Lessalc <lucianoclessa@gmail.com>
2022-10-09 20:22:56 -0300

    Methods to insert, update and deleted created

--- src/main/java/com/lessalc/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lessalc/workshopmongo/resources/UserResource.java
@@ -1,17 +1,21 @@
 package com.lessalc.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lessalc.workshopmongo.domain.User;
 import com.lessalc.workshopmongo.dto.UserDTO;
@@ -36,6 +40,28 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // Retorna o endereço da URI
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/com/lessalc/workshopmongo/services/UserService.java
+++ src/main/java/com/lessalc/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lessalc.workshopmongo.domain.User;
+import com.lessalc.workshopmongo.dto.UserDTO;
 import com.lessalc.workshopmongo.repository.UserRepository;
 import com.lessalc.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,32 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
 	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
+	}
+	
+	private User updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		return newObj;
+	}
+	
+	
+	
 }
