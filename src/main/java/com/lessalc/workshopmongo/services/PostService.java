d7045df64b556db4118c6cf20d34e7cff39e9624
Lessalc <lucianoclessa@gmail.com>
2022-10-13 23:53:31 -0300

    Adding Query Methods to the Post Repository
    Created queries with the default value and created with Query annotation

--- src/main/java/com/lessalc/workshopmongo/services/PostService.java
+++ src/main/java/com/lessalc/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.lessalc.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +24,13 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
 	}
 	
+	public List<Post> findByTitle(String text){
+		return repository.searchTitle(text);
+	}
 
-	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return repository.fullSearch(text, minDate, maxDate);
+	}
 	
 }
